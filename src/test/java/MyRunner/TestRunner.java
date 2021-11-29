a19bad4ad7a06a9875f8678409ba094474090b04
Tampara Ashwini <ashwinitampara@gmail.com>
2021-11-29 10:39:01 +0530

    first commit

---
+++ src/test/java/MyRunner/TestRunner.java
@@ -0,0 +1,26 @@
+package MyRunner;
+import org.junit.runner.RunWith;
+
+
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+
+
+
+features = "./src/test/java/Feature",
+
+
+
+glue = "StepDefination"
+
+
+
+)
+
+public class TestRunner {
+
+}
